c668ec27c001c46a3c21d12f54a872ee775515f9
Apris3 <turaamahapris@gmail.com>
2023-03-20 01:26:45 +0800

    replace image profile, update profile description, replace background color header and footer, and add style the hamburger-menu

--- src/data/data.js
+++ src/data/data.js
@@ -3,13 +3,15 @@ const projects = [
         project: 'Search Film',
         image : './images/image/Search-film.png',
         description : 'Project ini dibuat berdasarkan data API dari The Open Movie Data Base (OMDb API) dengan bahasa pemrograman javascript dan CSS responsive. Link project : ',
-        link : "http://search-film-project.000webhostapp.com/"
+        link : 'http://search-film-project.000webhostapp.com/',
+        linkGithub: 'https://github.com/Apris3/find-movies-project'
     },
     {
         project: 'Search Weather',
         image : './images/image/Search-weather.png',
         description : 'Project ini dibuat berdasarkan data API dari OpenWeatherMap dan dengan bahasa pemrograman javascript. Link project : ',
-        link : "https://tangerine-chaja-9728ba.netlify.app/"
+        link : "https://tangerine-chaja-9728ba.netlify.app/",
+        linkGithub: 'https://github.com/Apris3/project_search_weather'
     }
 ]
 
--- src/template/template-projects.js
+++ src/template/template-projects.js
@@ -4,7 +4,7 @@ const template = (e)=>{
             <img src="${e.image}" width="100%" class="img"/>
             <div class="detailProject">
                 <h4 class="titleProject">${e.project}</h4>
-                <p class="description">${e.description}<a href="${e.link}">${e.project}</a></p>
+                <p class="description">${e.description}<a href="${e.link}">${e.project}</a>, link github : <a href="${e.linkGithub}">Link github</a></p>
             </div>
         </div>
     `
